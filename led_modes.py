abbb05d33ada832f1fb391d2c734cb1ec643f9d7
Deathfireofdoom <Oskar.Elvkull@gmail.com>
2021-10-27 18:35:31 +0200

    Added test file

--- led_modes.py
+++ led_modes.py
@@ -115,8 +115,8 @@ class LedStrip(object):
         while x < times:
             print(x)
             for i in range(speed):
-                color = (int(color[0] - i * color_step[0] / speed), int(color[1] - i * color_step[1] / speed),
-                         int(color[2] - i * color_step[2] / speed), int(color[3] - i * color_step[3] / speed))
+                color = (max(int(color[0] - i * color_step[0] / speed)), max(int(color[1] - i * color_step[1] / speed)),
+                         max(int(color[2] - i * color_step[2] / speed)), max(int(color[3] - i * color_step[3] / speed)))
                 self.light(color)
                 time.sleep(0.01)
 
