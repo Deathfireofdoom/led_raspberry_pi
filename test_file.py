0f5adacba199ee85369ac3fc5ff02a389a183f92
Deathfireofdoom <Oskar.Elvkull@gmail.com>
2021-10-27 18:15:05 +0200

    Added test file

---
+++ test_file.py
@@ -0,0 +1,14 @@
+from led_modes import LedStrip
+
+
+LED_STRIP = LedStrip()
+
+COLOR1 = (0, 255, 0, 0)
+COLOR2 = (0, 0, 255, 0)
+COLOR3 = (255, 0, 0, 0)
+COLOR4 = (0, 0, 0, 255)
+
+
+
+if __name__ == '__main__':
+    LED_STRIP.pulse(COLOR1, COLOR4)
