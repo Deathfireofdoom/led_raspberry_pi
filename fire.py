b0c6cc4bc5cf7bf9ff44e3ad5825d73e94918e67
Deathfireofdoom <Oskar.Elvkull@gmail.com>
2021-10-30 14:24:24 +0200

    added fire

--- fire.py
+++ fire.py
@@ -16,7 +16,7 @@ class Fire(object):
     def burn(self):
         for i, spark in enumerate(self.sparks):
             color = spark.next_color()
-            color = tuple(int(c) for c in color)
+            color = tuple(min(max(int(c), 0), 255) for c in color)
             self.strip.setPixelColor(i, Color(*color))
         time.sleep(0.01)
         self.strip.show()
