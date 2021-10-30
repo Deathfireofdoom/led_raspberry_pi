e2488d12fc63e7c064db19231a16c27aea1793ff
Deathfireofdoom <Oskar.Elvkull@gmail.com>
2021-10-30 22:27:15 +0200

    hange

--- realistic_flame.py
+++ realistic_flame.py
@@ -1,5 +1,6 @@
 import random
 import numpy as np
+from time import sleep
 
 
 
@@ -13,16 +14,20 @@ class Flame(object):
         self.cells = np.zeros(size)
         print(self.cells)
 
-    def burn(self, spark_cells=[0, 1, 2], threshold=0.5, explosion_heat=[200, 300]):
+    def burn(self, spark_cells=[0, 1, 2], threshold=0.5, explosion_heat=[1500, 1600]):
+        self.cells[random.sample(spark_cells, 1)] = random.randint(*explosion_heat)
+        self.cells[random.sample(spark_cells, 1)] = random.randint(*explosion_heat)
+        self.cells[random.sample(spark_cells, 1)] = random.randint(*explosion_heat)
         while True:
-            if random.random() < threshold:
-                self.cells[random.sample(spark_cells, 1)] = random.randint(*explosion_heat)
+            #if random.random() < threshold:
+            #    self.cells[random.sample(spark_cells, 1)] = random.randint(*explosion_heat)
             self.calculate_temperature()
 
     def calculate_temperature(self, span=2):
         for i in range(len(self.cells)):
-            self.cells[i] = np.average(self.cells[max(0, i-span):span]) - self.cooling
-
+            new_temp = np.average(self.cells[max(0, i-span):i + span])
+            self.cells[i] = max(0, new_temp - self.cooling) if not np.isnan(new_temp) else 0.0
+        print(self.cells)
 
 
 
